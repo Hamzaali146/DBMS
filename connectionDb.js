872cd6cc6c0ecd6d8d48c73386ca124fbe8e8718
Hamzaali146 <aaly.galy@gmail.com>
2024-03-08 02:49:35 -0800

    connected_mysql

--- connectionDb.js
+++ connectionDb.js
@@ -1,7 +1,24 @@
-let mysql = require("mysql");
-let connection = mysql.createConnection({
+let mysql = require("mysql2");
+let connection = mysql.createPool({
     host: "127.0.0.1",
     user: "root",
     password: "familyhk1",
-    database:"se_dbms"
+    database:"SE_DBMS",
+    port:"3306",
+    waitForConnections: true,
+    connectionLimit : 10,
+    queueLimit:0
 });
+
+connection.query('SELECT * FROM Patrons', (error, results, fields) => {
+    if (error) {
+      console.error('Error connecting to MySQL:', error);
+      return;
+    }
+  
+    console.log('Query results:', results);
+  });
+// connection.connect(function(err){
+//     if(err) throw err;
+//     console.log("Connection Successfull")
+// })
