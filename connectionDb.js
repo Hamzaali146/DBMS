5f5824d34a407e848ec42cfd26664c07ee04b786
Hamzaali146 <aaly.galy@gmail.com>
2024-03-14 17:51:13 +0500

    updated_panel

--- connectionDb.js
+++ connectionDb.js
@@ -1,24 +1,43 @@
-let mysql = require("mysql2");
-let connection = mysql.createPool({
-    host: "127.0.0.1",
-    user: "root",
-    password: "familyhk1",
-    database:"SE_DBMS",
-    port:"3306",
-    waitForConnections: true,
-    connectionLimit : 10,
-    queueLimit:0
-});
+// let mysql = require("mysql2");
+// let connection = mysql.createPool({
+//     host: "156.67.73.151",
+//     user: "u481156254_hamza",
+//     password: "/a10UEI#3T",
+//     database:"u481156254_Copcompanion",
+//     port:"3306",
+//     waitForConnections: true,
+//     connectionLimit : 10,
+//     queueLimit:0
+// });
 
-connection.query('SELECT * FROM Patrons', (error, results, fields) => {
-    if (error) {
-      console.error('Error connecting to MySQL:', error);
-      return;
-    }
-  
-    console.log('Query results:', results);
-  });
-// connection.connect(function(err){
-//     if(err) throw err;
-//     console.log("Connection Successfull")
-// })
+// import e = require("express");
+
+// let obj = []
+// connection.query('SELECT * FROM officers', (error, results, fields) => {
+//     if (error) {
+//       console.error('Error connecting to MySQL:', error);
+//       return;
+//     }
+//     obj = results
+//     connection.end()
+//     console.log(obj)
+//     // console.log('Query results:', results);
+//   });
+//   // console.log(obj)
+// // connection.connect(function(err){
+// //     if(err) throw err;
+// //     console.log("Connection Successfull")
+// // })
+
+let a = {
+  name:"hamza",
+  age: 19,
+  major:"computer systems"
+
+}
+for (const key in a) {
+  if (a.hasOwnProperty.call(a, key)) {
+    const element = a[key];
+    console.log(element);
+  }
+}
--- main.js
+++ main.js
@@ -1,9 +1,36 @@
-const ejs = require('ejs')
-const express = require('express')
+// const ejs = require('ejs')
+import express from "express";
+import bodyParser from "body-parser";
 const app = express()
 const port = 3000
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+import mysql from "mysql2";
+let connection = mysql.createPool({
+    host: "127.0.0.1",
+    user: "root",
+    password: "familyhk1",
+    database:"copcompanion",
+    port:"3306",
+    waitForConnections: true,
+    connectionLimit : 10,
+    queueLimit:0
+});
+let obj = []
+connection.query('SELECT * FROM officers', (error, results, fields) => {
+    if (error) {
+      console.error('Error connecting to MySQL:', error);
+      return;
+    }
+    obj = results
+    console.log('Query results:', obj);
+  });
+// connection.connect(function(err){
+//     if(err) throw err;
+//     console.log("Connection Successfull")
+// })
+
 
 app.get('/', (req, res) => {
     let projectName = "Police Management System"
@@ -14,6 +41,19 @@ app.get('/database', (req, res) => {
     res.render("database")
 })
 
+app.post('/submit', (req, res) => {
+  let answer = req.body.answer;
+  let usName =req.body.usName;
+  // console.log(typeof(answer))
+  for (const key in obj) {
+    const element = obj[key];
+    if(element.firstName.toLowerCase()==usName.toLowerCase() && element.officerID === parseInt(answer)){
+      res.render("admin")
+    }
+    
+  }
+  res.render("index")
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
