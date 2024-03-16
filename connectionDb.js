adb049b7a9fc650f2db6ed960e9f4a99d038354d
Hamzaali146 <aaly.galy@gmail.com>
2024-03-16 16:39:53 +0500

    hostinger_IP

--- connectionDb.js
+++ connectionDb.js
@@ -1,43 +1,23 @@
-// let mysql = require("mysql2");
-// let connection = mysql.createPool({
-//     host: "156.67.73.151",
-//     user: "u481156254_hamza",
-//     password: "/a10UEI#3T",
-//     database:"u481156254_Copcompanion",
-//     port:"3306",
-//     waitForConnections: true,
-//     connectionLimit : 10,
-//     queueLimit:0
-// });
+import mysql from "mysql2";
+let connection = mysql.createPool({
+    host: "156.67.73.151",
+    user: "u481156254_hamza",
+    password: "/a10UEI#3T",
+    database:"u481156254_Copcompanion",
+    port:"3306",
+    waitForConnections: true,
+    connectionLimit : 10,
+    queueLimit:0
+});
 
-// import e = require("express");
-
-// let obj = []
-// connection.query('SELECT * FROM officers', (error, results, fields) => {
-//     if (error) {
-//       console.error('Error connecting to MySQL:', error);
-//       return;
-//     }
-//     obj = results
-//     connection.end()
-//     console.log(obj)
-//     // console.log('Query results:', results);
-//   });
-//   // console.log(obj)
-// // connection.connect(function(err){
-// //     if(err) throw err;
-// //     console.log("Connection Successfull")
-// // })
-
-let a = {
-  name:"hamza",
-  age: 19,
-  major:"computer systems"
-
-}
-for (const key in a) {
-  if (a.hasOwnProperty.call(a, key)) {
-    const element = a[key];
-    console.log(element);
-  }
-}
+let obj = []
+connection.query(`SELECT * FROM OFFICERS WHERE OFFICER_RANK = "Lieutenant" `, (error, results, fields) => {
+    if (error) {
+      console.error('Error connecting to MySQL:', error);
+      return;
+    }
+    obj = results
+    connection.end()
+    console.log(obj)
+    console.log('Query results:', results);
+  });
