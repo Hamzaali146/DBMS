b856640e337fff57c32abfe2a11b58acf0909404
Hamzaali146 <aaly.galy@gmail.com>
2024-03-22 15:37:36 +0500

    frontend doing

--- public/hiring_backend.js
+++ public/hiring_backend.js
@@ -56,32 +56,52 @@ function addOfficer(){
     </select> 
     </div>
 
-    <button type="submit">LOGIN
+    <button type="submit">Add To Database
         </button>
 
 </form>`
 }
-function myFunction(){
-    let officerIdValue = document.getElementById("officerId").value;
-    let deptIdValue = document.getElementById("deptId").value;
-    let badgeNumValue = document.getElementById("badgeNum").value;
-    let firstNumValue = document.getElementById("firstNum").value;
-    let lastNumValue = document.getElementById("lastNum").value;
-    let ContactNumValue = document.getElementById("ContactNum").value;
-    let emailValue = document.getElementById("email").value;
-    let dobValue = document.getElementById("dob").value;
-    let hiringDateValue = document.getElementById("hiringDate").value;
-    let officerRankValue = document.getElementById("officerRank").value;
-//     let sql = `INSERT INTO OFFICERS VALUES(${parseInt(officerIdValue)},${parseInt(deptIdValue)},${badgeNumValue},${firstNumValue},${lastNumValue},${officerRankValue},${ContactNumValue},${emailValue},${dobValue},${hiringDateValue})`
-//     connection.query(sql, (error, results, fields) => {
-//     if (error) {
-//       console.error('Error connecting to MySQL:', error);
-//       return;
-//     }
-//     // obj = results
-//     console.log("Row Inserted Successfully");
-//   });
-    console.log(officerIdValue,typeof(parseInt(officerIdValue)))
-    console.log(deptIdValue,typeof(parseInt(deptIdValue)))
-    console.log(ContactNumValue,typeof(parseInt(ContactNumValue)))
+
+function addDepartment(){
+  document.getElementById("formWindow").innerHTML = ""
+  document.getElementById("formWindow").innerHTML = `<form class="container" action="/departmentdata" method="post">
+  <div class="horizontal-container">
+    <h3 class="sitetitle">
+      Add Department in CopCompanion Database!
+    </h3>
+    
+  </div>
+  </h1> 
+
+  <div class="answer-container" >
+    <input type="text" name="postalCode" id="officerId" placeholder="Postal Code" autocomplete="off" >
+  </div>
+  <div class="answer-container" >
+    <input type="text" name="deptName" id="deptId" placeholder="deptName" autocomplete="off" >
+  </div>
+  <div class="answer-container" >
+    <input type="text" name="Location" id="badgeNum" placeholder="Location" autocomplete="off" >
+  </div>
+  <div class="answer-container" >
+    <input type="text" name="landLine" id="firstNum" placeholder="Land Line Number"autocomplete="off" >
+  </div>
+  <button type="submit">Add To Database
+      </button>
+
+</form>`
 }
+// function myFunction(){
+//     let officerIdValue = document.getElementById("officerId").value;
+//     let deptIdValue = document.getElementById("deptId").value;
+//     let badgeNumValue = document.getElementById("badgeNum").value;
+//     let firstNumValue = document.getElementById("firstNum").value;
+//     let lastNumValue = document.getElementById("lastNum").value;
+//     let ContactNumValue = document.getElementById("ContactNum").value;
+//     let emailValue = document.getElementById("email").value;
+//     let dobValue = document.getElementById("dob").value;
+//     let hiringDateValue = document.getElementById("hiringDate").value;
+//     let officerRankValue = document.getElementById("officerRank").value;
+//     console.log(officerIdValue,typeof(parseInt(officerIdValue)))
+//     console.log(deptIdValue,typeof(parseInt(deptIdValue)))
+//     console.log(ContactNumValue,typeof(parseInt(ContactNumValue)))
+// }
