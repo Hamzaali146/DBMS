2963e7fae1e99ad21063933f888291e7efa4a394
Hamza Ali <aaly.galy@gmail.com>
2024-06-24 09:24:43 -0700

    updated backend

--- public/hiring_backend.js
+++ public/hiring_backend.js
@@ -4,9 +4,7 @@ function addOfficer(){
     document.getElementById("formWindow").innerHTML = ""
     document.getElementById("formWindow").innerHTML = `<form class="container" action="/officerdata" method="post">
     <div class="horizontal-container">
-      <h3 class="sitetitle">
-        Add officer in CopCompanion Database!
-      </h3>
+    
       
     </div>
     </h1> 
@@ -97,17 +95,56 @@ function addDepartment(){
   </h1> 
 
   <div class="answer-container" >
-    <input type="text" name="postalCode" id="officerId" placeholder="Postal Code" autocomplete="off" >
-  </div>
-  <div class="answer-container" >
     <input type="text" name="deptName" id="deptId" placeholder="deptName" autocomplete="off" >
   </div>
+
   <div class="answer-container" >
-    <input type="text" name="Location" id="badgeNum" placeholder="Location" autocomplete="off" >
+    <input type="text" name="postalCode" id="officerId" placeholder="Postal Code" autocomplete="off" >
   </div>
+  
   <div class="answer-container" >
     <input type="text" name="landLine" id="firstNum" placeholder="Land Line Number"autocomplete="off" >
   </div>
+  <div class="answer-container" >
+    <input type="number" name="numOfficers" id="firstNum" placeholder="Numbers of Officer in department"autocomplete="off" >
+  </div>
+  <div class="answer-container">
+      <select name="areaId" id="areaId" required>
+        <option value="default" selected>--- Choose Area ID ---</option>
+        <option value="10200">10200 - Sector F-3, Mirpur, Azad Kashmir</option>
+        <option value="13100">13100 - Upper Chattar, Muzaffarabad, Azad Kashmir</option>
+        <option value="15100">15100 - Jutial, Gilgit, Gilgit-Baltistan</option>
+        <option value="16100">16100 - Airport Road, Skardu, Gilgit-Baltistan</option>
+        <option value="19200">19200 - Saidu Sharif, Swat, Khyber Pakhtunkhwa</option>
+        <option value="22010">22010 - Jinnahabad, Abbottabad, Khyber Pakhtunkhwa</option>
+        <option value="25000">25000 - Hayatabad, Peshawar, Khyber Pakhtunkhwa</option>
+        <option value="25100">25100 - University Town, Peshawar, Khyber Pakhtunkhwa</option>
+        <option value="44000">44000 - Blue Area, Islamabad, Punjab</option>
+        <option value="44004">44004 - Blue Area, Islamabad, Punjab</option>
+        <option value="44010">44010 - Blue Area, Islamabad, Punjab</option>
+        <option value="44040">44040 - Blue Area, Islamabad, Punjab</option>
+        <option value="44060">44060 - F-6, Islamabad Capital Territory, Islamabad</option>
+        <option value="44070">44070 - G-9, Islamabad Capital Territory, Islamabad</option>
+        <option value="44170">44170 - G-9, Islamabad Capital Territory, Islamabad</option>
+        <option value="45700">45700 - Bahria Town, Rawalpindi, Punjab</option>
+        <option value="46000">46000 - Sadar, Rawalpindi, Punjab</option>
+        <option value="54000">54000 - Gulberg, Lahore, Punjab</option>
+        <option value="54500">54500 - Johar Town, Lahore, Punjab</option>
+        <option value="54700">54700 - Model Town, Lahore, Punjab</option>
+        <option value="71000">71000 - Latifabad, Hyderabad, Sindh</option>
+        <option value="75600">75600 - Clifton, Karachi, Sindh</option>
+        <option value="75606">75606 - Clifton, Karachi, Sindh</option>
+        <option value="87200">87200 - Sariab, Quetta, Balochistan</option>
+        <option value="87202">87202 - Sariab, Quetta, Balochistan</option>
+        <option value="87220">87220 - Sariab, Quetta, Balochistan</option>
+        <option value="87300">87300 - Cantonment, Quetta, Balochistan</option>
+        <option value="87305">87305 - Cantonment, Quetta, Balochistan</option>
+        <option value="89000">89000 - Model Town, Khuzdar, Balochistan</option>
+        <option value="89900">89900 - Model Town, Khuzdar, Balochistan</option>
+        <option value="91200">91200 - Port Area, Gwadar, Balochistan</option>
+      </select>
+    </div>
+
   <button type="submit">Add To Database
       </button>
 
